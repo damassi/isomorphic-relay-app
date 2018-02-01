ee542871b1b96f97f8208dce32a7b63585bdc492
Christopher Pappas <damassi.pappas@gmail.com>
2018-02-01 01:29:31 -0800

    Implement network-layer

--- relay-modern/src/lib/isomorphic-relay/getRelayEnvironment.js
+++ relay-modern/src/lib/isomorphic-relay/getRelayEnvironment.js
@@ -1,23 +1,21 @@
 import 'isomorphic-fetch'
 import 'regenerator-runtime/runtime'
-import { Environment, Network, RecordSource, Store } from 'relay-runtime'
+import { Environment, RecordSource, Store } from 'relay-runtime'
 
-function fetchQuery(operation, variables) {
-  return fetch(process.env.METAPHYSICS_BASE_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      query: operation.text,
-      variables
-    })
-  }).then((response) => {
-    return response.json()
-  })
-}
+import {
+  RelayNetworkLayer,
+  urlMiddleware,
+  loggerMiddleware,
+  gqlErrorsMiddleware,
+  perfMiddleware
+} from 'react-relay-network-modern'
 
-const network = Network.create(fetchQuery)
+const network = new RelayNetworkLayer([
+  urlMiddleware({ url: process.env.METAPHYSICS_BASE_URL }),
+  loggerMiddleware(),
+  gqlErrorsMiddleware(),
+  perfMiddleware()
+])
 
 export function getRelayEnvironment(records) {
   const source = new RecordSource(records)
